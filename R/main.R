c53275708791a7a3536ca1478cc297c5735ef071
JGarnica22 <j.garnica22@gmail.com>
2024-11-25 16:56:39 +0100

    add: function to create main object

--- R/main.R
+++ R/main.R
@@ -1,3 +1,39 @@
+# Create scTypeEval object
+
+create.scTypeEval <- function(matrix, 
+                              metadata = NULL, 
+                              gene.lists = list(), 
+                              black.list = character(), 
+                              active.ident = NULL) {
+   
+   # Check input type
+   if (inherits(matrix, "Seurat")) {
+      counts <- as(matrix@assays$RNA@counts, "dgCMatrix")
+      metadata <- as.data.frame(matrix@meta.data)
+   } else if (inherits(matrix, "SingleCellExperiment")) {
+      counts <- as(assay(matrix, "counts"), "dgCMatrix")
+      metadata <- as.data.frame(colData(matrix))
+   } else if (inherits(matrix, "matrix") || inherits(matrix, "dgCMatrix")) {
+      counts <- as(matrix, "dgCMatrix")
+      if (is.null(metadata)) {
+         stop("For matrix input, metadata dataframe must be provided.")
+      }
+      metadata <- as.data.frame(metadata)
+   } else {
+      stop("Input object must be a Seurat, SingleCellExperiment, or matrix-like object.")
+   }
+   
+   # Create the scTypeEval object
+   scTypeEval_obj <- new("scTypeEval",
+                         counts = counts,
+                         metadata = metadata,
+                         norm.param = norm.param,
+                         gene.lists = gene.lists,
+                         black.list = black.list,
+                         version = version)
+   
+   return(scTypeEval_obj)
+}
 
 
 
