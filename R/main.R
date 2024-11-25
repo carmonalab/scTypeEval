17a9690ff321bcca966e0c009fadabbfb86db34b
JGarnica22 <j.garnica22@gmail.com>
2024-11-25 14:41:49 +0100

    add: function to add normalization parameters

--- R/main.R
+++ R/main.R
@@ -1,21 +1,30 @@
-# Compute distances if not provided
-if (metric %in% c("silhouette", "ward", "modularity")) {
-   dist <- get.distance(mat,
-                        norm.mat,
-                        dist.method = dist.method)  # Example distance calculation
-}
 
-# Compute centroids if not provided
-if (metric %in% c("inertia", "Xie-Beni", "S_Dbw", "I")) {
-   centroids <- compute_centroids(norm.mat, ident)
-}
 
-# Compute inertia if not provided
-if (metric %in% c("inertia", "Xie-Beni")) {
-   inertia <- compute_inertia(norm.mat, ident, centroids)
-}
 
-# Compute min centroid distance if not provided
-if (metric %in% c("inertia", "Xie-Beni")) {
-   inertia <- compute_inertia(norm.mat, ident, centroids)
+
+
+# join function to get params for normalization
+add.Normalization_params <- function(scTypeEval,
+                                     method = c("Log1p", "CLR", "pearson"),
+                                     margin = 2L,
+                                     size_factors = TRUE,
+                                     return_value = F){
+   
+   mat <- scTypeEval@counts
+   
+   # Run the requested methods
+   norm_params <- switch(method[1],
+                         "Log1p" = get.SumCounts(mat, margin),
+                         "CLR" = get.GeoMean(dist, ident, KNNGraph_k),
+                         "pearson" = transformGamPoi:::.handle_size_factors(size_factors, mat),
+                         stop(metric, " is not a supported normalization method.")
+   )
+   
+   if(return_value){
+      return(norm_params)
+   } else {
+      scTypeEval@norm.param[[method]] <- norm_params
+      return(scTypeEval)
+   }
+   
 }
