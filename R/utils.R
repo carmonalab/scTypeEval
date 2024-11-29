378c84c84544b9d154310ca5902802d28bede07d
JGarnica22 <j.garnica22@gmail.com>
2024-11-29 13:36:29 +0100

    fix: split.matrix

--- R/utils.R
+++ R/utils.R
@@ -32,42 +32,81 @@ set_parallel_params <- function(ncores = NULL,
 }
 
 
+split.matrix <- function(mat,
+                         ident,
+                         sample = NULL,
+                         min.cells = 10,
+                         bparam = BiocParallel::SerialParam()){
+   if (length(sample) != ncol(mat)) {
+      stop("The length of 'sample' must match the number of columns in 'mat'")
+   }
+   
+   # Get the unique sample groups
+   unique_samples <- levels(sample)
+   
+   # Initialize an empty list to store matrices
+   split_list <- list()
+   
+   # Loop through each unique sample and subset the matrix
+   split_list <- BiocParallel::bplapply(unique_samples,
+                                        BPPARAM = bparam,
+                                        function(s){
+                                           new.mat <- mat[, sample == s, drop = FALSE]
+                                           new.ident <- ident[sample == s]
+                                           
+                                           ret <- valid.sc(mat = new.mat,
+                                                           ident = new.ident,
+                                                           min.cells = min.cells)
+                                           
+                                           return(ret)
+                                        })
+   names(split_list) <- as.character(unique_samples)
+   
+   return(split_list)
+}
+
+
 consistency.helper <- function(mat,
                               ident,
                               normalization.method,
+                              distance.method = "euclidean",
                               IntVal.metric,
                               data.type,
-                              sample = NULL){
+                              sample = NULL,
+                              bparam = BiocParallel::SerialParam(),
+                              min.samples = 5,
+                              min.cells = 10){
    
    mat <- get.matrix(mat,
                      data.type = data.type,
                      ident = ident,
                      sample = sample,
                      min.samples = min.samples,
-                     min.cells = min.cells)
-   
-   
-   
+                     min.cells = min.cells,
+                     bparam = bparam)
+
    if(!is.list(mat)){
-      mat <- as.list(mat)
+      mat <- list(mat)
    }
    
-   consist <- lapply(mat,
-                     function(red.mat){
-                        
-                        # normalized subetted matrix
-                        norm.mat <- Normalize_data(red.mat[["matrix"]],
-                                                   method = normalization.method)
-                        
-                        # compute internal validation metrics
-                        con <- calculate_IntVal_metric(mat = red.mat[["matrix"]],
-                                                       norm.mat = norm.mat,
-                                                       metrics = IntVal.metric,
-                                                       dist.method = distance.method,
-                                                       ident = red.mat[["ident"]])
-                        return(con)
-                        
-                     })
+   consist <- BiocParallel::bplapply(mat,
+                                     BPPARAM = bparam,
+                                     function(red.mat){
+                                        
+                                        # normalized subetted matrix
+                                        norm.mat <- Normalize_data(red.mat@matrix,
+                                                                   method = normalization.method)
+                                        
+                                        
+                                        # compute internal validation metrics
+                                        con <- calculate_IntVal_metric(mat = red.mat@matrix,
+                                                                       norm.mat = norm.mat,
+                                                                       metrics = IntVal.metric,
+                                                                       dist.method = distance.method,
+                                                                       ident = red.mat@ident)
+                                        return(con)
+                                        
+                                     })
    
    names(consist) <- names(mat)
    
@@ -83,8 +122,7 @@ consistency.helper <- function(mat,
       }
       consist <- join
    }
-   
-   names(consist) <- IntVal.metric
+
    
    return(consist)
    
