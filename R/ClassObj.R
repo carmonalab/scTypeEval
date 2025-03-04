437a2429dcbf1905897461689bcc688d40f90bd0
JGarnica22 <j.garnica22@gmail.com>
2025-03-04 11:25:15 +0100

    fix: counts as dgCMatrix

--- R/ClassObj.R
+++ R/ClassObj.R
@@ -1,7 +1,7 @@
 # S4 class object
 methods::setClass("scTypeEval",
                   slots = c(
-                     counts = "matrix", # raw counts
+                     counts = "dgCMatrix", # raw counts
                      metadata = "data.frame", # data.frame with metadata
                      consistency = "list", # actual consistency results assays
                      gene.lists = "list", # list of either HGV, markers
