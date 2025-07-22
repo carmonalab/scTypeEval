63046021dc8365d6f1f3618fe66c39bc0fdf1887
JGarnica22 <j.garnica22@gmail.com>
2025-07-22 18:58:42 +0200

    build distance-like matrix

--- R/BestHit.R
+++ R/BestHit.R
@@ -66,9 +66,9 @@ get.DEG_logfc <- function(mat,
 # test and ref must be sparse matrices
 spear_classify <- function(test_mat,
                            ref_mat,
+                           ref.ident,
                            min.markers = 10,
                            bparam = BiocParallel::SerialParam()){
-   ref.ident <- ref_mat@ident
    
    # Step 0: ensure overlapping genes
    common_genes <- intersect(rownames(test), rownames(ref))
@@ -128,7 +128,7 @@ singleR.helper <- function(test,
                            ref,
                            ref.ident,
                            bparam = BiocParallel::SerialParam()){
-
+   
    # SingleR prediction
    pred <- SingleR::SingleR(
       test = test,
@@ -173,7 +173,7 @@ score.tidy <- function(pred,
    pred2 <- score.tidy(pred2, true2)
    
    pred <- dplyr::inner_join(pred1, pred2, by = "true") |>
-      dplyr::mutate(score = score.x * score.y) |>
+      dplyr::mutate(score = (1-score.x) * (1-score.y)) |>
       dplyr::select(true, score) |>
       dplyr::rename("celltype" = "true")
    
@@ -185,7 +185,7 @@ match.tidy <- function(pred, .true){
       dplyr::select("label") |>
       dplyr::mutate(true = .true,
                     score = ifelse(true == label,
-                                   1, 0
+                                   0, 1
                     )
       )
    
@@ -208,94 +208,41 @@ match.tidy <- function(pred, .true){
    return(pred)
 }
 
-get.MutualMatrix <- function(mat,
-                             ident,
-                             sample,
-                             data.type = "sc",
-                             min.cells = 10,
-                             min.samples = 5,
-                             sep = "_",
-                             bparam = BiocParallel::SerialParam()){
-   # Combined grouping
-   groups <- factor(paste(sample, ident, sep = sep))
-   
-   valid_indices <- valid.indices(groups = groups,
-                                  ident = ident,
-                                  sample = sample,
-                                  min.samples = min.samples,
-                                  min.cells = min.cells,
-                                  sep = sep)
-   
-   mat <- mat[, valid_indices, drop = FALSE]
-   
-   mats <- split_matrix(mat,
-                        ident = ident[valid_indices],
-                        sample = sample[valid_indices],
-                        min.cells = min.cells,
-                        bparam = bparam)
-   
-   mat.split <- lapply(mats,
-                       function(mat){
-                          get.matrix(mat@matrix,
-                                     data.type = data.type,
-                                     ident = mat@ident,
-                                     sample = NULL, # now is pseudobulk per cell type per invidividual sample
-                                     min.cells = min.cells,
-                                     bparam = bparam)
-                       })
-   
-   return(mat.split)
-}
+
 
 
 bestHit <- function(mat,
                     ident,
                     ident_GroundTruth = NULL,
                     sample,
-                    method = "Mutual.Score",
-                    classifier = "Spearman_correlation",
-                    data.type = "sc",
-                    min.cells = 10,
-                    min.samples = 5,
-                    sep = "_",
+                    method = "Score",
+                    classifier = "SingleR",
                     ncores = 1,
                     bparam = NULL,
-                    progressbar = TRUE){
+                    progressbar = TRUE,
+                    verbose = TRUE){
    
    param <- set_parallel_params(ncores = ncores,
                                 bparam = bparam,
                                 progressbar = progressbar)
-   
-   mat.split <- get.MutualMatrix(mat = mat,
-                                 ident = ident,
-                                 sample = sample,
-                                 data.type = data.type,
-                                 min.cells = min.cells,
-                                 min.samples = min.samples,
-                                 sep = sep,
-                                 bparam = param)
+   # split by sample
+   mat.split <- split_matrix(mat = mat,
+                             ident = ident,
+                             sample = sample,
+                             bparam = param)
    
    if(is.null(ident_GroundTruth)){
       mat.splitGT <- mat.split
    } else {
-      mat.splitGT <- get.MutualMatrix(mat = mat,
-                                      ident = ident_GroundTruth,
-                                      sample = sample,
-                                      data.type = data.type,
-                                      min.cells = min.cells,
-                                      min.samples = min.samples,
-                                      sep = sep,
-                                      bparam = param)
+      mat.splitGT <- split_matrix(mat = mat,
+                                  ident = ident_GroundTruth,
+                                  sample = sample,
+                                  bparam = param)
    }
    
    
-   names_list <- names(mat.split)
-   idx <- expand.grid(i = seq_along(names_list),
-                      j = seq_along(names_list)) |>
-      filter(i < j)
-   
-   combis <- data.frame(Var1 = names_list[idx$i],
-                        Var2 = names_list[idx$j])
+   n <- length(mat.split)
+   combs <- utils::combn(n, 2, simplify = FALSE)
    
    # set classifier
    classifier <- switch(classifier,
@@ -304,86 +251,72 @@ bestHit <- function(mat,
                         stop(classifier, " classifier not supported")
    )
    
-   df.tmp <- list()
-   
-   df.tmp <- BiocParallel::bplapply(seq_len(nrow(combis)),
+   if(verbose){message("Computing pairwise BesHiT similarity... \n")}
+   df.tmp <- BiocParallel::bplapply(combs,
                                     BPPARAM = param,
                                     function(i) {
-      a <- combis[i, 1]
-      b <- combis[i, 2]
-      
-      # Check that both reference and query are not NULL
-      if (is.null(mat.split[[a]]) || is.null(mat.split[[b]]) ||
-          is.null(mat.splitGT[[a]]) || is.null(mat.splitGT[[b]])) {
-         return(NULL)
-      }
-      
-      # Check if each matrix has at least two unique cell types
-      nc <- lapply(mat.splitGT[c(a, b)], function(x) length(unique(x@ident)))
-      if (any(nc < 2)) {
-         return(NULL)
-      }
-      
-      # Perform classification
-      pred1 <- classifier(mat.split[[a]], mat.splitGT[[b]])
-      pred2 <- classifier(mat.split[[b]], mat.splitGT[[a]])
-      
-      true1 <- mat.split[[a]]@ident
-      true2 <- mat.split[[b]]@ident
-      
-      # Evaluate metrics
-      results <- lapply(method, function(meth) {
-         switch(meth,
-                "Mutual.Score" = .score(pred1 = pred1,
-                                        pred2 = pred2,
-                                        true1 = true1,
-                                        true2 = true2),
-                "Mutual.Match" = .match(pred1 = pred1,
-                                        pred2 = pred2,
-                                        true1 = true1,
-                                        true2 = true2),
-                stop(meth, " is not a supported Mutual Besthit method."))
-      })
-      names(results) <- method
-      return(results)
-   })
+                                       a <- i[1]
+                                       b <- i[2]
+                                       
+                                       # Check that both reference and query are not NULL
+                                       if (is.null(mat.split[[a]]) || is.null(mat.split[[b]]) ||
+                                           is.null(mat.splitGT[[a]]) || is.null(mat.splitGT[[b]])) {
+                                          return(NULL)
+                                       }
+                                       
+                                       # Check if each matrix has at least two unique cell types
+                                       nc <- lapply(mat.splitGT[c(a, b)], function(x) length(unique(x@ident)))
+                                       if (any(nc < 2)) {
+                                          return(NULL)
+                                       }
+                                       
+                                       # Perform classification
+                                       pred1 <- classifier(mat.split[[a]], mat.splitGT[[b]])
+                                       pred2 <- classifier(mat.split[[b]], mat.splitGT[[a]])
+                                       
+                                       true1 <- mat.splitGT[[a]]@ident
+                                       true2 <- mat.splitGT[[b]]@ident
+                                       
+                                       # Evaluate metrics
+                                       results <- switch(tolower(method),
+                                                 "Score" = .score(pred1 = pred1,
+                                                                  pred2 = pred2,
+                                                                  true1 = true1,
+                                                                  true2 = true2),
+                                                 "Match" = .match(pred1 = pred1,
+                                                                  pred2 = pred2,
+                                                                  true1 = true1,
+                                                                  true2 = true2),
+                                                 stop(meth, " is not a supported Mutual Besthit method."))
+                                       
+                                       results <- results |>
+                                          dplyr::mutate(i = paste(names(mat.split)[a], celltype, sep = "_"),
+                                                        j = paste(names(mat.split)[b], celltype, sep = "_"))
+                                       
+                                       lst <- apply(results, 1, as.list)
+                                       
+                                       # return a data frame with one column with cell type and one column with score
+                                       return(lst)
+                                    })
    
    # Filter out NULLs
    df.tmp <- Filter(Negate(is.null), df.tmp)
    
-   # join results by method
-   resli <- lapply(method, function(m)
-   {
-      res <- do.call(rbind, lapply(df.tmp, function(x) x[[m]]))
-      # summarize results per cell type
-      res <- res |>
-         dplyr::group_by(celltype) |>
-         dplyr::summarize(score = mean(score, na.rm = T))
-      
-      if(m == "Mutual.Match"){
-         # normalize for BestHiT match
-         nident <- sapply(mat.split, function(mat) {length(mat@ident)})
-         # Generate all pairwise combinations of the samples
-         sample_combinations <- combn(nident, 2)
-         # Calculate pairwise probabilities (1 / (cell types in sample i * cell types in sample j))
-         probabilities <- apply(sample_combinations, 2, function(pair) {
-            1 / (pair[1] * pair[2])})
-         # Average all pairwise probabilities together
-         min.val <- mean(probabilities) 
-         res <- res |>
-            dplyr::mutate(score = minmax_norm(score,
-                                              min_value = min.val,
-                                              max_value = 1))
-      }
-      # extract vector with output
-      res <-  res |>
-         dplyr::pull(score, name = celltype)
-      
-      return(res)
-   }
-   )
-   names(resli) <- method
+   # ---- Build pairwise matrix with lists ----
+   n <- length(unlist(df.tmp))
+   rcn <- lapply(df.tmp, function(x){x[["i"]]}) |> unique()
+   # Initialize distance matrix
+   dist_mat <- matrix(0, n, n)
+   rownames(dist_mat) <- colnames(dist_mat) <- rcn
    
-   return(resli)
+   # Fill in symmetric matrix
+   for (res in df.tmp) {
+      i <- res$i
+      j <- res$j
+      d <- res$score
+      dist_mat[i, j] <- dist_mat[j, i] <- d
+   }
+   
+   return(as.dist(dist_mat))
    
 }
