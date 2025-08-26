f71e6b3d172f8fd948461c638384a21d2f72f4d9
JGarnica22 <j.garnica22@gmail.com>
2025-08-26 11:17:54 +0200

    fix: name param

--- R/IntClusVal.R
+++ R/IntClusVal.R
@@ -248,7 +248,7 @@ compute_averageSimilarity <- function(dist, ident) {
       avg_other_dist <- mean(dist_mat[i, other_group_idx])
       
       # Compute normalized score: higher is better (self-similar and dissimilar to others)
-      similarity_vector[i] <- 1 - (avg_own_dist / (avg_own_dist + avg_other_dist))
+      ratio_vector[i] <- 1 - (avg_own_dist / (avg_own_dist + avg_other_dist))
    }
    
    # Aggregate: mean per cluster
