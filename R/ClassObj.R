aff21323943c2797b8e7d345410c7cbf7a9d08a4
JGarnica22 <j.garnica22@gmail.com>
2025-12-19 10:21:14 +0100

    add: consistency slot in scTypeEval

--- R/ClassObj.R
+++ R/ClassObj.R
@@ -10,6 +10,7 @@ methods::setClass("scTypeEval",
                      black.list = "ANY", # list of genes in black list
                      active.ident = "ANY", # default grouping variable
                      reductions = "list", # list of dim reductions
+                     consistency = "list", # list of data frames with obtained consistencies
                      version = "character" # package version
                   )
 )
@@ -37,12 +38,18 @@ methods::setMethod("initialize", "scTypeEval", function(.Object, ...) {
       args$reductions <- list()
    }
    
+   # Set default for consistency if not provided
+   if (is.null(args$consistency)) {
+      args$consistency <- list()
+   }
+   
    # Pass the updated arguments to the default initialize method
    .Object <- callNextMethod(.Object, ..., 
                              data = args$data,
                              dissimilarity = args$dissimilarity,
                              consistency = args$consistency, 
-                             reductions = args$reductions)
+                             reductions = args$reductions,
+                             consistency = args$consistency)
    
    validObject(.Object)  # Validate the object
    .Object
