58076c2f32c49db9638df6775eb036be273d7c8f
JGarnica22 <j.garnica22@gmail.com>
2026-01-27 11:04:32 +0100

    add: whole package base docs imports

---
+++ R/scTypeEval-package.R
@@ -0,0 +1,6 @@
+#' @keywords internal
+#' @importFrom grDevices dev.off pdf
+#' @importFrom methods as callNextMethod new validObject
+#' @importFrom stats aggregate anova as.dist complete.cases cor kmeans lm median quantile sd setNames t.test weighted.mean
+#' @importFrom utils capture.output getFromNamespace head install.packages packageVersion
+"_PACKAGE"
